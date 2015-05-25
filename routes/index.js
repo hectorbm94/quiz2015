e9e3597bb96b036699f9c44cbf2df3fd3f92fdc1
hectorbm94 <hectorbm94@gmail.com>
2015-05-25 19:39:44 +0200

    Editor de preguntas

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,8 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/author',   quizController.author);
 router.get('/quizes/new',   quizController.new);
 router.post('/quizes/create',  quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',  quizController.update);
 
 // Busqueda
 router.get('/search', quizController.busqueda);
