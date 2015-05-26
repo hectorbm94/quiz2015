6f29f96bb6af5124fea9aeb7322007fef3dfa5d6
hectorbm94 <hectorbm94@gmail.com>
2015-05-26 20:25:05 +0200

    Mis preguntas e imagenes

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -31,11 +31,16 @@ exports.load = function(req, res, next, quizId) {
   ).catch(function(error){next(error)});
 };
 
-// GET /quizes/
-exports.index = function(req, res) {
-   models.Quiz.findAll().then(function(quizes){
-     res.render('quizes/index', {quizes: quizes, errors: []});
-   }).catch(function(error){next(error)});
+// GET /users/:userId/quizes
+exports.index = function(req, res) {  
+  var options = {};
+  if(req.user){
+    options.where = {UserId: req.user.id}
+  }
+  
+  models.Quiz.findAll(options).then(
+     function(quizes) {
+       res.render('quizes/index.ejs', {quizes: quizes, errors: []})});
 };
 
 // GET /quizes/:id
@@ -71,13 +76,16 @@ exports.new = function(req, res) {
 // POST /quizes/create
 exports.create = function(req, res) {
   req.body.quiz.UserId = req.session.user.id;
+  if(req.files.image){
+   req.body.quiz.image = req.files.image.name;
+  }
   var quiz = models.Quiz.build( req.body.quiz );
 
     quiz.validate().then(function(err){
       if (err) {
          res.render('quizes/new', {quiz: quiz, errors: err.errors});
       } else { // save: guarda en DB campos pregunta y respuesta de quiz
-        quiz.save({fields: ["pregunta", "respuesta", "UserId"]}).then( function(){res.redirect('/quizes')}) 
+        quiz.save({fields: ["pregunta", "respuesta", "UserId", "image"]}).then( function(){res.redirect('/quizes')}) 
       }      // res.redirect: Redirección HTTP a lista de preguntas
     }).catch(function(error){next(error)});
 }; 
@@ -91,6 +99,9 @@ exports.edit = function(req, res) {
 
 // PUT /quizes/:id
 exports.update = function(req, res) {
+  if(req.files.image){
+     req.quiz.image = req.files.image.name;
+  }
   req.quiz.pregunta  = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
 
@@ -100,7 +111,7 @@ exports.update = function(req, res) {
        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
       } else {
         req.quiz     // save: guarda campos pregunta y respuesta en DB
-        .save( {fields: ["pregunta", "respuesta"]})
+        .save( {fields: ["pregunta", "respuesta", "image"]})
         .then( function(){ res.redirect('/quizes');});
       }     // Redirección HTTP a lista de preguntas (URL relativo)
     }
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var multer = require('multer');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
@@ -28,6 +29,7 @@ router.post('/user',  userController.create);     // registrar usuario
 router.get('/user/:userId(\\d+)/edit',  sessionController.loginRequired, userController.ownershipRequired, userController.edit);// editar información de cuenta
 router.put('/user/:userId(\\d+)',  sessionController.loginRequired, userController.ownershipRequired, userController.update);     // actualizar información de cuenta
 router.delete('/user/:userId(\\d+)',  sessionController.loginRequired, userController.ownershipRequired, userController.destroy);     // borrar cuenta
+router.get('/user/:userId(\\d+)/quizes',  quizController.index);     // ver las preguntas de un usuario
 
 // definicion de rutas de quizes
 router.get('/quizes', quizController.index);
@@ -35,9 +37,10 @@ router.get('/quizes/:quizId(\\d+)',   quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/author',   quizController.author);
 router.get('/quizes/new',  sessionController.loginRequired, quizController.new);
-router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.post('/quizes/create', sessionController.loginRequired,
+multer({ dest: './public/media/'}), quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',  sessionController.loginRequired, quizController.ownershipRequired ,quizController.edit);
-router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.ownershipRequired, quizController.update);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.ownershipRequired, multer({ dest: './public/media/'}), quizController.update);
 router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.ownershipRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
