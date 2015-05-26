71adc6032cf475ec4ced7602b781c194a393b397
hectorbm94 <hectorbm94@gmail.com>
2015-05-26 22:25:49 +0200

    favoritos v2

--- controllers/favourites_controller.js
+++ controllers/favourites_controller.js
@@ -18,23 +18,22 @@ exports.load = function(req, res, next, userId,quizId) {
 // GET /users/:userId/favourites
 exports.index = function(req, res) {  
 
-   models.Favourite.findAll({ where:{UserId: Number(req.user.id)}}).then(
+   models.Favourites.findAll({ where:{UserId: Number(req.params.userId)}}).then(
      function(quizes) {
        res.render('quizes/index.ejs', {quizes: quizes, errors: []})});
-};
-
-//FAV
-exports.fav = function(req, res){
-	req.user.hasQuiz(req.quiz.id).then(function(result) {
-		if (!result) {
-			req.user.addQuiz(req.quiz.id);
-		}
-	});
-	res.redirect("/quizes");
 };
 
 // DELETE
 exports.delete = function(req, res){
 	models.Favourites.destroy({where:{ UserId: Number(req.user.id), QuizId: Number(req.quiz.id) }});
 	res.redirect("/quizes");
+};
+
+//FAV
+exports.fav = function(req, res){
+	//req.body.quiz.UserId = req.session.user.id;
+	var fav = models.Favourites.build(	//crea objeto fav
+    {UserId: req.session.user.id, QuizId:req.params.quizId}
+  );
+  fav.save({fields: ["UserId", "QuizId"]}).then( function(){res.redirect('/quizes')})
 };
